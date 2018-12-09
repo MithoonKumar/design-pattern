ab801c9e1938ac2f1a12b3f73d63a11a13441fcc
Mithoon Kumar <mithoonk@zeta.tech>
2018-12-09 14:11:04 +0530

    Adapter Design Pattern

---
+++ src/AdapterDesignPattern/AdpaterClass.java
@@ -0,0 +1,11 @@
+package AdapterDesignPattern;
+
+public class AdpaterClass implements Pen {
+
+    private PilotPen pilotPen =  new PilotPen();
+
+    @Override
+    public void write(String str) {
+        pilotPen.mark(str);
+    }
+}
---
+++ src/AdapterDesignPattern/Main.java
@@ -0,0 +1,9 @@
+package AdapterDesignPattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Pen p = new AdpaterClass();
+        p.write("Hello World ! I learnt using adpater");
+    }
+}
